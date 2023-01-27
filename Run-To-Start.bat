eb386c49b33026b9180cece44c879f97d018488a
Pamda <pro80105@gmail.com>
2023-01-27 20:28:08 +0300

    first commit

---
+++ Run-To-Start.bat
@@ -0,0 +1,7 @@
+color a 
+title goodbye pc 
+cls
+
+:a
+start "New Window" cmd /c troll.cmd
+goto a
---
+++ troll.cmd
@@ -0,0 +1,18 @@
+@echo off
+title mes is mid
+:a
+color 4
+echo Mes is mid
+goto b
+
+:b
+color b
+echo MEs iS miD
+goto c
+
+:c 
+color 5
+echo mEs is Mid 
+goto a
+
+
